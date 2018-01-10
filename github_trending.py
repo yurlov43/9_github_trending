8078dd6bd1b1e52bc4093bc18946b5d083dfbc22
Master <master@iMac-Master.local>
2018-01-10 22:52:08 +0300

    Get-parameters requests encodes independently

--- github_trending.py
+++ github_trending.py
@@ -1,16 +1,17 @@
 import requests
-import json
 import datetime
 
 
 def get_trending_repositories(top_size, days_ago):
     created_date = datetime.date.today() - datetime.timedelta(days_ago)
-    request_link = (
-        'https://api.github.com/search/repositories?q=created:>'
-        '{}&sort=stars&order=desc'.
-        format(created_date.strftime('%Y-%m-%d'))
-    )
-    return requests.get(request_link).json()['items'][:top_size]
+    parameters_in_link = {
+        'q': 'created:>{}'.format(created_date.strftime('%Y-%m-%d')),
+        'sort': 'stars',
+        'order': 'desc'}
+    request_link = 'https://api.github.com/search/repositories'
+    return requests.get(
+        request_link,
+        params=parameters_in_link).json()['items'][:top_size]
 
 
 def get_open_issues_amount(repository_owner, repository_name):
@@ -21,7 +22,8 @@ def get_open_issues_amount(repository_owner, repository_name):
     return requests.get(request_link).json()
 
 
-def print_data_to_console(number, repository_name, repository_url, amount_issues):
+def print_data_to_console(
+        number, repository_name, repository_url, amount_issues):
     print('{}{}.{}Name: {}'.format('\n', number, '\t', repository_name))
     print('{}Url: {}'.format('\t', repository_url))
     print('{}Amount issues: {}{}'.format('\t', amount_issues, '\n'))
